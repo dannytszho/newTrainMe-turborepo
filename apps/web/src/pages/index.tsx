c0599eef33a0540b934b47eedf8ed7f8e0ce4e2d
dannytszho <dannytszho@gmail.com>
2022-05-27 18:29:18 -0700

    Add animation in about section

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -22,17 +22,23 @@ const Home: NextPage = () => {
       </section>
       {/* About section */}
       <section id="about" className="px-20 md:px-64 p-96 mx-auto bg-blue-200 text-center">
-        Take Your Training to the Next Level
+        <p className='text-sm md:text-lg'>Take Your Training to the Next Level!</p>
         <br />
-        Train Me is an app for everything you can do on your own to improve as a soccer player. Whether you’re just getting started or are an elite player with pro aspirations, we’ll guide you in that process.
+        Train Me is an app for everything you can do on your own to improve as a soccer player.    Whether you’re just getting started or are an elite player with pro aspirations, we’ll guide you in that process.
       </section>
       {/* Animate section */}
       <section className="h-[500px] mx-auto bg-red-200 text-center">
         <div className='mx-40 py-5 border-white border-b-4'></div>
-        <div className='mx-40 py-10 border-white border-b-4 text-2xl'>Train Me</div>
+        <div className='mx-40 py-10 border-white border-b-4'><div className='bg-clip-text text-[100px] font-bold text-transparent bg-gradient-to-l from-purple-700 via-red-500 to-yellow-200 animate-[textclip_2s_linear_infinite]'>Train Me</div></div>
         <div className='flex'>
           <div className='mx-60 w-36 h-36 p-10 bg-white rounded-full'></div>
-          <div className='mx-40 text-[40px] '>If You have a Pro mentality.</div>
+          <div className='text-[40px]'>
+            If You have a 
+            <div className='h-[50px] overflow-hidden inline-block whitespace-pre'>
+              <div className='animate-[show_5s_linear_infinite]'> Pro </div>
+              <div> D1 </div>
+            </div>
+            mentality.</div>
         </div>
         <div className='mx-40 py-10 border-white border-t-4 text-2xl' />
         <br />
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -6,7 +6,50 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      Animation: {
+        textclip: 'textclip 2s linear infinite',
+        show: 'show 4s linear infinite'
+      },
+      keyframes: {
+        textclip: {
+          '0%, 100%': {
+            'background-size':'200% 200%',
+             'background-position': 'left center'
+         },
+         '50%': {
+            'background-size':'200% 200%',
+             'background-position': 'right center'
+         }
+        },
+        show: {
+          '0%': {
+            'margin-top':'0px'
+          },
+          '5% ': {
+            'margin-top':'0px'
+          },
+          '33%': {
+            'margin-top':'0px'
+          },
+          '38%': {
+            'margin-top':'-90px'
+          },
+          '66%': {
+            'margin-top':'0px'
+          },
+          '71%': {
+            'margin-top':'0px'
+          },
+          '99.99%': {
+            'margin-top':'0px'
+          },
+          '100%': {
+            'margin-top':'-270px'
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
