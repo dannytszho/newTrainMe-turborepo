d16ea19deb65b73f4d379a9ad1e258e91a02764f
dannytszho <dannytszho@gmail.com>
2022-06-14 10:31:21 -0700

    Use public embeded google map

--- packages/ui/public/common/GoogleMaps.tsx
+++ packages/ui/public/common/GoogleMaps.tsx
@@ -6,7 +6,7 @@ const GoogleMaps = () => {
       height="220"
       allowFullScreen
       loading="lazy"
-      src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ1RoHdmFX2YARrRCfMSfDz9c&key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}`}
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3354.3722112855758!2d-116.99019514938584!3d32.782388690925686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d9576176071ad5%3A0xd7cfc327319f10ad!2sGrossmont%20High%20School!5e0!3m2!1sen!2sus!4v1655227484727!5m2!1sen!2sus"
     />
   )
 }
