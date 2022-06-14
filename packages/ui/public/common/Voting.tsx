16621ee8eb44b4d1abd9d141f72bec5f9b802fed
dannytszho <dannytszho@gmail.com>
2022-06-14 15:43:03 -0700

    Hide buttons after voting

--- packages/ui/public/common/Voting.tsx
+++ packages/ui/public/common/Voting.tsx
@@ -7,7 +7,7 @@ import { collection, addDoc } from 'firebase/firestore/lite'
 const Voting = () => {
   const { isLoaded, userId, sessionId, getToken } = useAuth()
   const { session } = useSession()
-  const [disable, setDisable] = useState(false)
+  const [show, setShow] = useState(true)
 
   const addVoteDocument = async (vote: string, user: string) => {
     await addDoc(collection(db, 'votes'), {
@@ -16,26 +16,36 @@ const Voting = () => {
     })
   }
   console.log(session)
+  console.log(show)
   return (
     <>
       <div className="h-full text-black flex flex-col justify-center items-center gap-2">
         <h1>Game this week</h1>
-        <button
-          className="bg-green-400 cursor-pointer rounded-full text-white drop-shadow-lg w-24 h-8"
-          onClick={() =>
-            addVoteDocument('In', session.publicUserData.identifier)
-          }
-        >
-          In
-        </button>
-        <button
-          className="bg-green-400 cursor-pointer rounded-full text-white drop-shadow-lg w-24 h-8"
-          onClick={() =>
-            addVoteDocument('Out', session.publicUserData.identifier)
-          }
-        >
-          Out
-        </button>
+        {show ? (
+          <button
+            className="bg-green-400 cursor-pointer rounded-full text-white drop-shadow-lg w-24 h-8"
+            onClick={() => {
+              addVoteDocument('In', session.publicUserData.identifier)
+              setShow(false)
+            }}
+          >
+            In
+          </button>
+        ) : (
+          'See you on Sunday'
+        )}
+        {show ? (
+          <button
+            className="bg-green-400 cursor-pointer rounded-full text-white drop-shadow-lg w-24 h-8"
+            onClick={() =>
+              addVoteDocument('Out', session.publicUserData.identifier)
+            }
+          >
+            Out
+          </button>
+        ) : (
+          'Hope to see you next game'
+        )}
       </div>
     </>
   )
